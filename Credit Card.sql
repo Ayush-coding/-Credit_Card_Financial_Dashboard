5794439a9051f38ce3b489854a3e3e63cb5832a3
Ayush <145697699+Ayush-coding@users.noreply.github.com>
2026-02-12 15:45:05 +0530

    Add files via upload

---
+++ Credit Card.sql
@@ -0,0 +1,51 @@
+CREATE TABLE CC_DETAILS(
+
+Client_Num INT,
+    Card_Category VARCHAR(20),
+    Annual_Fees INT,
+    Activation_30_Days INT,
+    Customer_Acq_Cost INT,
+    Week_Start_Date DATE,
+    Week_Num VARCHAR(20),
+    Qtr VARCHAR(10),
+    current_year INT,
+    Credit_Limit DECIMAL(10,2),
+    Total_Revolving_Bal INT,
+    Total_Trans_Amt INT,
+    Total_Trans_Ct INT,
+    Avg_Utilization_Ratio DECIMAL(10,3),
+    Use_Chip VARCHAR(10),
+    Exp_Type VARCHAR(50),
+    Interest_Earned DECIMAL(10,3),
+    Delinquent_Acc VARCHAR(5)
+);
+
+-- 2. Create cc_detail table
+
+CREATE TABLE cust_detail (
+    Client_Num INT,
+    Customer_Age INT,
+    Gender VARCHAR(5),
+    Dependent_Count INT,
+    Education_Level VARCHAR(50),
+    Marital_Status VARCHAR(20),
+    State_cd VARCHAR(50),
+    Zipcode VARCHAR(20),
+    Car_Owner VARCHAR(5),
+    House_Owner VARCHAR(5),
+    Personal_Loan VARCHAR(5),
+    Contact VARCHAR(50),
+    Customer_Job VARCHAR(50),
+    Income INT,
+    Cust_Satisfaction_Score INT
+);
+
+
+SELECT * FROM cc_details
+SELECT * FROM cust_detail
+
+-- COPY cc_details
+-- FROM '‪C:\credit_card.csv'
+-- -- FROM '‪C:\Users\91900\Desktop\drive-download-20260211T191325Z-1-001\credit_card.csv'
+-- DELIMITER ','
+-- CSV HEADER
